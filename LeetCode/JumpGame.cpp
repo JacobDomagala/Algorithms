61732cc0c13f6b6ec064accdb25602ee1dc75afb
Jakub Domagala <domagala.jacob@gmail.com>
2020-05-18 11:23:02 +0200

    Add problems descriptions with examples

--- LeetCode/JumpGame.cpp
+++ LeetCode/JumpGame.cpp
@@ -3,6 +3,21 @@
 
 using namespace std;
 
+/*
+    Given an array of non-negative integers, you are initially positioned at the first index of the array.
+
+    Each element in the array represents your maximum jump length at that position.
+
+    Determine if you are able to reach the last index.
+
+    -----------------------------------EXAMPLE-----------------------------------
+
+    Input: nums = [2,3,1,1,4]
+    Output: true
+    Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+
+*/
+
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
